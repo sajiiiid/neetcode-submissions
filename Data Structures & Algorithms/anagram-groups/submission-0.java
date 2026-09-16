21a70a34b25ce3813a1b4e09e81d676f36138da3
Sajid ELHADDADY <elhaddadysajid@gmail.com>
2026-09-16 15:44:34 +0100

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            // 1. Create the canonical key (sorted version of word)
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+
+            // 2. Insert into existing bucket or create a new one
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        // 3. Wrap all map values directly into the required List<List<String>>
+        return new ArrayList<>(map.values());
+    }
+}
